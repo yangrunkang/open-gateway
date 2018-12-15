d214ad5a0f7c9816cb41411fbfea236340c5bdb9
yangrunkang <1743703238@qq.com>
2018-12-16 07:22:56 +0800

    Mod: from spring.io to init spring boot project

---
+++ open-basic-user/src/main/java/com/open/basic/user/openbasicuser/OpenBasicUserApplication.java
@@ -0,0 +1,14 @@
+package com.open.basic.user.openbasicuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBasicUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBasicUserApplication.class, args);
+	}
+
+}
+
