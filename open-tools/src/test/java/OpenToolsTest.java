3f012f73a8894f20487aa0633b022bf6dc97c3f9
yangrunkang <1743703238@qq.com>
2019-01-02 18:24:14 +0800

    Add: add mybatis-generator plugin

---
+++ open-tools/src/test/java/OpenToolsTest.java
@@ -0,0 +1,11 @@
+/**
+ * Description:
+ * All rights Reserved, Designed By HQYG
+ *
+ * @author Yangrunkang
+ * @Copyright: Copyright(C) 2016
+ * @Company: HQYG
+ * @CreateDate: 2019/1/2 16:36
+ */
+public class OpenToolsTest {
+}
