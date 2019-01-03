fe22644d1f91280f0330870405e54a323bdb1fac
yangrunkang <1743703238@qq.com>
2019-01-03 18:18:45 +0800

    Add: 注释规范

---
+++ docs/standard.md
@@ -0,0 +1,10 @@
+# 规范
+
+### 注释
+类注释
+/**
+ *
+ * <p>All rights Reserved, Designed By YangRunkang.</p>
+ * @author $user$
+ * @date $date$ $time$
+ */
--- open-basic-user/src/main/java/com/open/basic/user/OpenBasicUserApplication.java
+++ open-basic-user/src/main/java/com/open/basic/user/OpenBasicUserApplication.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 开放-用户服务-启动类
+ * <p>All rights Reserved, Designed By YangRunkang.</p>
+ * @author Yangrunkang
+ * @date 2019/1/3 14:32
+ */
 @SpringBootApplication
 @MapperScan("com.open.basic.user.mapper")
 public class OpenBasicUserApplication {
