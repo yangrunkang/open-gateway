10f160bbcd6197fe72750755cb4044ecfc773fda
yangrunkang <1743703238@qq.com>
2019-01-04 00:08:35 +0800

    Add: add UserService

---
+++ open-spi-user/src/main/java/com/open/spi/user/UserService.java
@@ -0,0 +1,17 @@
+package com.open.spi.user;
+
+/**
+ * 用户服务
+ * <p>All rights Reserved, Designed By YangRunkang.</p>
+ * @author runkangyang
+ * @date 2019/1/3 11:34 PM
+ */
+public interface UserService {
+
+    /**
+     * 打招呼
+     * @return
+     */
+    String sayHello();
+
+}
